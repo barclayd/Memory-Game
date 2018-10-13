81a3afb0a44ab849e3dd5bf47e1a05628bfc9c71
Daniel Barclay <daniel@urbanintel.io>
2018-10-13 20:14:57 +0100

    setTimeout function for clear card flipping. Refactoring to follow

--- scripts.js
+++ scripts.js
@@ -21,8 +21,22 @@ function flipCard() {
         // });
 
         // check that cards match
-        console.log(firstCard.dataset.framework);
-        console.log(secondCard.dataset.framework);
+        // console.log(firstCard.dataset.framework);
+        // console.log(secondCard.dataset.framework);
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            // the cards match!
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                firstCard.classList.remove('flip');
+            }, 1500);
+
+        }
+
+        console.log('Function executed');
     }
 }
 
